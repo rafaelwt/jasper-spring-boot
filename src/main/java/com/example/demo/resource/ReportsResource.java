cb5d40b5945feeaa572d8a7b8500a35b3705527c
Rafael <rafael.wt@gmail.com>
2019-04-18 17:21:22 -0400

    validacion de errores

--- src/main/java/com/example/demo/resource/ReportsResource.java
+++ src/main/java/com/example/demo/resource/ReportsResource.java
@@ -6,10 +6,12 @@ import java.util.Properties;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.InputStream;
 import java.sql.Connection;
@@ -29,173 +31,199 @@ import net.sf.jasperreports.export.SimpleWriterExporterOutput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
 import net.sf.jasperreports.export.SimplePdfExporterConfiguration;
 import net.sf.jasperreports.export.SimplePdfReportConfiguration;
+
 @RestController
 @RequestMapping("/rest/reports")
 public class ReportsResource {
-	public ReportsResource() {	
-	}
+  public ReportsResource() {
+  }
 
-	 @GetMapping("/gastosdiarios")
-	 public void gastosdiarios(HttpServletResponse response, @RequestParam String fechaini, @RequestParam String fechafin, @RequestParam String cod_caja) throws Exception {
-			 String url = "jdbc:postgresql://vps229753.vps.ovh.ca:5432/db_presupuesto";
-			 Properties props = new Properties();
-			 props.setProperty("user","postgres");
-			 props.setProperty("password","123456Zxcv");
-			 Class.forName("org.postgresql.Driver");
-			 Connection conn = DriverManager.getConnection(url, props);
-			 response.setContentType("application/pdf");
-			InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_gastos_diarios.jrxml");
-			JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+  @GetMapping("/gastosdiarios")
+  public void gastosdiarios(HttpServletResponse response, @RequestParam String fechaini, @RequestParam String fechafin,
+      @RequestParam String cod_caja) throws Exception {
+    try {
+      String url = "jdbc:postgresql://vps229753.vps.ovh.ca:5432/db_presupuesto";
+      Properties props = new Properties();
+      props.setProperty("user", "postgres");
+      props.setProperty("password", "123456Zxcv");
+      Class.forName("org.postgresql.Driver");
+      Connection conn = DriverManager.getConnection(url, props);
+      response.setContentType("application/pdf");
+      InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_gastos_diarios.jrxml");
+      JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
 
-			 HashMap params = new HashMap();
-       params.put("fec_ini", fechaini);
-       params.put("fec_fin", fechafin);
-       params.put("cod_caja", cod_caja);
-			
-       JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
-      // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, conn);
-			JRPdfExporter exporter = new JRPdfExporter();
-			 
-			exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-			// exporter.setExporterOutput(new SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la ruta raiz
-			exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream())); 
-			SimplePdfReportConfiguration reportConfig
-			  = new SimplePdfReportConfiguration();
-			reportConfig.setSizePageToContent(true);
-			reportConfig.setForceLineBreakPolicy(false);
-			 
-			SimplePdfExporterConfiguration exportConfig
-			  = new SimplePdfExporterConfiguration();
-			exportConfig.setMetadataAuthor("baeldung");
-			exportConfig.setEncrypted(true);
-			exportConfig.setAllowedPermissionsHint("PRINTING");
-			 
-			exporter.setConfiguration(reportConfig);
-			exporter.setConfiguration(exportConfig);
-			 
-			exporter.exportReport();
-        }
-        @GetMapping("/presupuestoasignado")
-        public void presupuestoasignado(HttpServletResponse response, @RequestParam Integer cod_mes, @RequestParam Integer cod_area,@RequestParam Integer cod_subarea) throws Exception {
-                
-                String url = "jdbc:postgresql://vps229753.vps.ovh.ca:5432/db_presupuesto";
-                Properties props = new Properties();
-                props.setProperty("user","postgres");
-                props.setProperty("password","123456Zxcv");
-                Class.forName("org.postgresql.Driver");
-                Connection conn = DriverManager.getConnection(url, props);
-                response.setContentType("application/pdf");
-               InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_presupuesto_segun_asignado.jrxml");
-               JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
-   
-                HashMap params = new HashMap();
-                params.put("cod_mes", cod_mes);
-                params.put("cod_area", cod_area);
-                params.put("cod_subarea", cod_subarea);
-              JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
-               // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, conn);
-               JRPdfExporter exporter = new JRPdfExporter();
-                
-               exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-               // exporter.setExporterOutput(new SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la ruta raiz
-               exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream())); 
-               SimplePdfReportConfiguration reportConfig
-                 = new SimplePdfReportConfiguration();
-               reportConfig.setSizePageToContent(true);
-               reportConfig.setForceLineBreakPolicy(false);
-                
-               SimplePdfExporterConfiguration exportConfig
-                 = new SimplePdfExporterConfiguration();
-               exportConfig.setMetadataAuthor("baeldung");
-               exportConfig.setEncrypted(true);
-               exportConfig.setAllowedPermissionsHint("PRINTING");
-                
-               exporter.setConfiguration(reportConfig);
-               exporter.setConfiguration(exportConfig);
-                
-               exporter.exportReport();
-           }
+      HashMap params = new HashMap();
+      params.put("fec_ini", fechaini);
+      params.put("fec_fin", fechafin);
+      params.put("cod_caja", cod_caja);
 
+      JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
 
-           @GetMapping("/presupuestoaprobado")
-           public void presupuestoaprobado(HttpServletResponse response, @RequestParam Integer cod_mes, @RequestParam Integer cod_area,@RequestParam Integer cod_subarea) throws Exception {
-                   
-                   String url = "jdbc:postgresql://vps229753.vps.ovh.ca:5432/db_presupuesto";
-                   Properties props = new Properties();
-                   props.setProperty("user","postgres");
-                   props.setProperty("password","123456Zxcv");
-                   Class.forName("org.postgresql.Driver");
-                   Connection conn = DriverManager.getConnection(url, props);
-                   response.setContentType("application/pdf");
-                  InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_presupuesto_segun_aprobado.jrxml");
-                  JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
-      
-                   HashMap params = new HashMap();
-                   params.put("cod_mes", cod_mes);
-                   params.put("cod_area", cod_area);
-                   params.put("cod_subarea", cod_subarea);
-                 JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
-                  // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, conn);
-                  JRPdfExporter exporter = new JRPdfExporter();
-                   
-                  exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-                  // exporter.setExporterOutput(new SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la ruta raiz
-                  exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream())); 
-                  SimplePdfReportConfiguration reportConfig
-                    = new SimplePdfReportConfiguration();
-                  reportConfig.setSizePageToContent(true);
-                  reportConfig.setForceLineBreakPolicy(false);
-                   
-                  SimplePdfExporterConfiguration exportConfig
-                    = new SimplePdfExporterConfiguration();
-                  exportConfig.setMetadataAuthor("baeldung");
-                  exportConfig.setEncrypted(true);
-                  exportConfig.setAllowedPermissionsHint("PRINTING");
-                   
-                  exporter.setConfiguration(reportConfig);
-                  exporter.setConfiguration(exportConfig);
-                   
-                  exporter.exportReport();
-              }
+      // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null,
+      // conn);
+      JRPdfExporter exporter = new JRPdfExporter();
 
-              @GetMapping("/presupuestoclasificacion")
-              public void presupuestoclasificacion(HttpServletResponse response, @RequestParam Integer cod_mes, @RequestParam Integer cod_clasificacion) throws Exception {
-                      
-                      String url = "jdbc:postgresql://vps229753.vps.ovh.ca:5432/db_presupuesto";
-                      Properties props = new Properties();
-                      props.setProperty("user","postgres");
-                      props.setProperty("password","123456Zxcv");
-                      Class.forName("org.postgresql.Driver");
-                      Connection conn = DriverManager.getConnection(url, props);
-                      response.setContentType("application/pdf");
-                     InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_presupuesto_por_clasificacion.jrxml");
-                     JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
-         
-                      HashMap params = new HashMap();
-                      params.put("cod_mes", cod_mes);
-                      params.put("cod_clasificacion", cod_clasificacion);
-                      
-                    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
-                     // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null, conn);
-                     JRPdfExporter exporter = new JRPdfExporter();
-                      
-                     exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-                     // exporter.setExporterOutput(new SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la ruta raiz
-                     exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream())); 
-                     SimplePdfReportConfiguration reportConfig
-                       = new SimplePdfReportConfiguration();
-                     reportConfig.setSizePageToContent(true);
-                     reportConfig.setForceLineBreakPolicy(false);
-                      
-                     SimplePdfExporterConfiguration exportConfig
-                       = new SimplePdfExporterConfiguration();
-                     exportConfig.setMetadataAuthor("baeldung");
-                     exportConfig.setEncrypted(true);
-                     exportConfig.setAllowedPermissionsHint("PRINTING");
-                      
-                     exporter.setConfiguration(reportConfig);
-                     exporter.setConfiguration(exportConfig);
-                      
-                     exporter.exportReport();
-                 }
+      exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+      // exporter.setExporterOutput(new
+      // SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la
+      // ruta raiz
+      exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+      SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+      reportConfig.setSizePageToContent(true);
+      reportConfig.setForceLineBreakPolicy(false);
+
+      SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+      exportConfig.setMetadataAuthor("baeldung");
+      exportConfig.setEncrypted(true);
+      exportConfig.setAllowedPermissionsHint("PRINTING");
+
+      exporter.setConfiguration(reportConfig);
+      exporter.setConfiguration(exportConfig);
+
+      exporter.exportReport();
+    } catch (Exception exc) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error al generar el reportte", null);
+    }
+
+  }
+
+  @GetMapping("/presupuestoasignado")
+  public void presupuestoasignado(HttpServletResponse response, @RequestParam Integer cod_mes,
+      @RequestParam Integer cod_area, @RequestParam Integer cod_subarea) throws Exception {
+    try {
+      String url = "jdbc:postgresql://vps229753.vps.ovh.ca:5432/db_presupuesto";
+      Properties props = new Properties();
+      props.setProperty("user", "postgres");
+      props.setProperty("password", "123456Zxcv");
+      Class.forName("org.postgresql.Driver");
+      Connection conn = DriverManager.getConnection(url, props);
+      response.setContentType("application/pdf");
+      InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_presupuesto_segun_asignado.jrxml");
+      JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
+      HashMap params = new HashMap();
+      params.put("cod_mes", cod_mes);
+      params.put("cod_area", cod_area);
+      params.put("cod_subarea", cod_subarea);
+      JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
+      // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null,
+      // conn);
+      JRPdfExporter exporter = new JRPdfExporter();
+
+      exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+      // exporter.setExporterOutput(new
+      // SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la
+      // ruta raiz
+      exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+      SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+      reportConfig.setSizePageToContent(true);
+      reportConfig.setForceLineBreakPolicy(false);
+
+      SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+      exportConfig.setMetadataAuthor("baeldung");
+      exportConfig.setEncrypted(true);
+      exportConfig.setAllowedPermissionsHint("PRINTING");
+
+      exporter.setConfiguration(reportConfig);
+      exporter.setConfiguration(exportConfig);
+
+      exporter.exportReport();
+    } catch (Exception exc) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error al generar el reportte", null);
+    }
+
+  }
+
+  @GetMapping("/presupuestoaprobado")
+  public void presupuestoaprobado(HttpServletResponse response, @RequestParam Integer cod_mes,
+      @RequestParam Integer cod_area, @RequestParam Integer cod_subarea) throws Exception {
+    try {
+      String url = "jdbc:postgresql://vps229753.vps.ovh.ca:5432/db_presupuesto";
+      Properties props = new Properties();
+      props.setProperty("user", "postgres");
+      props.setProperty("password", "123456Zxcv");
+      Class.forName("org.postgresql.Driver");
+      Connection conn = DriverManager.getConnection(url, props);
+      response.setContentType("application/pdf");
+      InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_presupuesto_segun_aprobado.jrxml");
+      JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
+      HashMap params = new HashMap();
+      params.put("cod_mes", cod_mes);
+      params.put("cod_area", cod_area);
+      params.put("cod_subarea", cod_subarea);
+      JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
+      // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null,
+      // conn);
+      JRPdfExporter exporter = new JRPdfExporter();
+
+      exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+      // exporter.setExporterOutput(new
+      // SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la
+      // ruta raiz
+      exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+      SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+      reportConfig.setSizePageToContent(true);
+      reportConfig.setForceLineBreakPolicy(false);
+
+      SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+      exportConfig.setMetadataAuthor("baeldung");
+      exportConfig.setEncrypted(true);
+      exportConfig.setAllowedPermissionsHint("PRINTING");
+
+      exporter.setConfiguration(reportConfig);
+      exporter.setConfiguration(exportConfig);
+
+      exporter.exportReport();
+    } catch (Exception exc) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error al generar el reportte", null);
+    }
+
+  }
+
+  @GetMapping("/presupuestoclasificacion")
+  public void presupuestoclasificacion(HttpServletResponse response, @RequestParam Integer cod_mes,
+      @RequestParam Integer cod_clasificacion) throws Exception {
+    try {
+      String url = "jdbc:postgresql://vps229753.vps.ovh.ca:5432/db_presupuesto";
+      Properties props = new Properties();
+      props.setProperty("user", "postgres");
+      props.setProperty("password", "123456Zxcv");
+      Class.forName("org.postgresql.Driver");
+      Connection conn = DriverManager.getConnection(url, props);
+      response.setContentType("application/pdf");
+      InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_presupuesto_por_clasificacion.jrxml");
+      JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
+      HashMap params = new HashMap();
+      params.put("cod_mes", cod_mes);
+      params.put("cod_clasificacion", cod_clasificacion);
+
+      JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
+      // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null,
+      // conn);
+      JRPdfExporter exporter = new JRPdfExporter();
+
+      exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+      // exporter.setExporterOutput(new
+      // SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la
+      // ruta raiz
+      exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+      SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+      reportConfig.setSizePageToContent(true);
+      reportConfig.setForceLineBreakPolicy(false);
+
+      SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+      exportConfig.setMetadataAuthor("baeldung");
+      exportConfig.setEncrypted(true);
+      exportConfig.setAllowedPermissionsHint("PRINTING");
+
+      exporter.setConfiguration(reportConfig);
+      exporter.setConfiguration(exportConfig);
+
+      exporter.exportReport();
+    } catch (Exception exc) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error al generar el reportte", null);
+    }
+  }
 }
