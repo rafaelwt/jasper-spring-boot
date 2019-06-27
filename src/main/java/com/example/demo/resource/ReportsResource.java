04eeccd1694ed8b51426e69af8626f64b9c1dfc3
Quispe Morales <lquispe@scz.prefectura.gov.bo>
2019-06-27 17:39:09 -0400

    a

--- src/main/java/com/example/demo/resource/ReportsResource.java
+++ src/main/java/com/example/demo/resource/ReportsResource.java
@@ -296,6 +296,53 @@ public class ReportsResource {
   }
 
   @CrossOrigin
+  @GetMapping("/presupuestoaprobadogerencial")
+  public void presupuestoaprobadogerencial(HttpServletResponse response, @RequestParam Integer cod_mes,
+      @RequestParam Integer cod_area, @RequestParam Integer cod_subarea) throws Exception {
+    try {
+      Properties props = new Properties();
+      props.setProperty("user", "postgres");
+      props.setProperty("password", contrasena);
+      Class.forName("org.postgresql.Driver");
+      Connection conn = DriverManager.getConnection(url, props);
+      response.setContentType("application/pdf");
+      InputStream inputStream = this.getClass().getResourceAsStream("/reports/rpt_presupuesto_segun_aprobado_gerencial.jrxml");
+      JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
+      HashMap params = new HashMap();
+      params.put("cod_mes", cod_mes);
+      params.put("cod_area", cod_area);
+      params.put("cod_subarea", cod_subarea);
+      JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, conn);
+      // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, null,
+      // conn);
+      JRPdfExporter exporter = new JRPdfExporter();
+
+      exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+      // exporter.setExporterOutput(new
+      // SimpleOutputStreamExporterOutput("userReport.pdf")); //genera un pdf en la
+      // ruta raiz
+      exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+      SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
+      reportConfig.setSizePageToContent(true);
+      reportConfig.setForceLineBreakPolicy(false);
+
+      SimplePdfExporterConfiguration exportConfig = new SimplePdfExporterConfiguration();
+      exportConfig.setMetadataAuthor("baeldung");
+      exportConfig.setEncrypted(true);
+      exportConfig.setAllowedPermissionsHint("PRINTING");
+
+      exporter.setConfiguration(reportConfig);
+      exporter.setConfiguration(exportConfig);
+
+      exporter.exportReport();
+    } catch (Exception exc) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error al generar el reporte", null);
+    }
+
+  }
+
+  @CrossOrigin
   @GetMapping("/presupuestoclasificacion")
   public void presupuestoclasificacion(HttpServletResponse response, @RequestParam Integer cod_mes,
       @RequestParam Integer cod_clasificacion) throws Exception {
